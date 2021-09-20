b4302046561bbf1fb3b2cc5d5d3af0a7040e6e8e
vksh-chandra <52464865+vksh-chandra@users.noreply.github.com>
2021-09-21 05:18:12 +0530

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,50 @@
+package Sorting;
+import java.util.*;
+public class QuickSort {
+    static int partition(int[] array, int lb, int ub){
+        int start = lb;
+        int pivot = array[lb]; 
+        int end = ub;
+
+        while(start<end){
+            while(array[start]<= pivot){
+                start++;
+            }
+            while(array[end]>pivot){
+                end--;
+            }
+            if(start<end){
+                int temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+            }
+        }
+        int temp = array[end];
+        array[end] = array[lb];
+        array[lb] = temp; 
+
+        return end;
+    }
+
+    static void quickSort(int[] array, int lb, int ub){
+        if(lb<ub){
+            int loc = partition(array, lb, ub);
+            quickSort(array, lb, loc-1);
+            quickSort(array, loc+1, ub);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n = src.nextInt();
+        System.out.println("Enter "+n + " elemnt in array: ");
+        int[] array = new int[n];
+        for(int i=0;i<n;i++){
+            array[i] = src.nextInt();
+        }
+
+        quickSort(array,0, n-1);
+        System.out.println(Arrays.toString(array));
+        src.close();
+    }
+}
